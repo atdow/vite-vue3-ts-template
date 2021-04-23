51b501810d27f76d580c3f524a449a35e5f4bcad
zhuwendong <zhuwendong@sinohealth.com>
2021-04-23 16:40:27 +0800

    chore: 暂时隐藏element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@
  * @Author: atdow
  * @Date: 2021-04-21 16:38:31
  * @LastEditors: null
- * @LastEditTime: 2021-04-21 16:51:05
+ * @LastEditTime: 2021-04-23 16:37:13
  * @Description: file content
  */
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+// import ElementPlus from 'element-plus'
+import App from './App.vue'
 
-import router from "./router/index";
-import store from "./store/index";
+import router from './router/index'
+import store from './store/index'
 
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+// import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(router).use(store).use(ElementPlus).mount("#app");
+// createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
