394ddf3e8d998e185c5ab453c4f3dc6727a34f1b
atdow <atdow@users.noreply.github.com>
2021-04-23 08:44:45 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 105901ac64b0f59067cece2755c8aad03fa57d2c

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: atdow
- * @Date: 2021-04-21 17:49:53
- * @LastEditors: null
- * @LastEditTime: 2021-04-21 17:49:54
- * @Description: file content
- */
-module.exports = { extends: ['@commitlint/config-conventional'] }
